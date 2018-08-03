bdd2b28b4f9a52a918bc75b7639c2596119150ed
gegyed <gabor.egyed@vertis.com>
2018-08-03 13:29:31 +0200

    first commit

---
+++ .env-files/create_dev_certificates.sh
@@ -0,0 +1,18 @@
+# bash
+set -e
+
+# generate CA private key
+openssl genrsa -out ca.key 2048
+
+# self signed CA certificate
+openssl req -x509 -new -nodes -subj "/commonName=ca" \
+       -key ca.key -sha256 -days 1024 -out ca.crt
+
+
+openssl genrsa -out certificate.key 2048
+openssl req -new -key certificate.key -out certificate.csr
+openssl x509 -req -in certificate.csr -CA ca.crt -CAkey ca.key \
+        -out certificate.crt -days 500 -sha256 \
+        -CAcreateserial -CAserial ca.srl
+
+rm ca.srl certificate.srl
